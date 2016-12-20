f38b62727e86533ef997a2907161af44b8ce3df6
CharmedSatyr <z@charmed.tech>
2016-12-21 08:10:32 +0900

    Reverting from IP geolocator to HTML since it didn't help the site load in an iFrame.

--- xy/sites/0004-show-the-local-weather/js/index.js
+++ xy/sites/0004-show-the-local-weather/js/index.js
@@ -1,7 +1,5 @@
 /*
 ISSUE TRACKER/Wish List
--Change geolocator to get US states/KR provinces without losing accuracy. How?
--Spell out country names once the geolocator is updated
 -Open Weather Map only provides an HTTPS API with paid accounts
 -Get better icons from https://erikflowers.github.io/weather-icons/
 */
@@ -45,190 +43,198 @@ function rangeOfColors(val) {
 }
 
 $(document).ready(function() {
-  var lat, lon;
-  //Geolocator based on IP address (HTML geolocator asks permission, which is great for privacy but annoying)
-  $.getJSON('https://ipinfo.io', function(data) {
-    var location = data.loc.split(',');
-    lat = location[0];
-    lon = location[1];
 
-    var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?lat=";
-    //Weather display
-    $.getJSON(weatherAPI + lat + "&lon=" + lon + "&APPID=" + "061f24cf3cde2f60644a8240302983f2", function(report) {
+  /*  //Geolocator based on IP address - if used, just tend to ending punctuation at the very end of the document
+    $.getJSON('https://ipinfo.io', function(data) {
+      var location = data.loc.split(',');
+      var lat = location[0];
+      var lon = location[1];
+  */
 
-      //City
-      var city = report.name;
-      if (city !== undefined) {
-        $('#city').html(city + ", ");
-      } else {
-        $('#city').html('Currently Unavailable, ');
-      }
-      //Country - only Korea will be spelled out
-      function spell(val) {
-        var a = "";
-        if (val === 'KR') {
-          a = 'South Korea';
+  //HTML Geolocator
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lon = position.coords.longitude;
+      var lat = position.coords.latitude;
+
+      //Weather display
+      $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=" + "061f24cf3cde2f60644a8240302983f2", function(report) {
+
+        //City
+        var city = report.name;
+        if (city !== undefined) {
+          $('#city').html(city + ", ");
         } else {
-          a = val;
+          $('#city').html('Currently Unavailable, ');
         }
-        return a;
-      }
-
-      var country = spell(report.sys.country);
-      if (country !== undefined) {
-        $('#country').html(country);
-      } else {
-        $('#country').html('Location Not Detected');
-      }
-      //Temperature
-      var temp = Math.round(report.main.temp - 273.15);
-
-      if (temp !== undefined) {
-
-        $('#tempC').html(temp + "&deg");
-        $('#tempF').html(Math.round(temp * 1.8 + 32) + "&deg");
-        $('#buttonM').html("C");
-        $('#buttonI').html("F");
-      } else {
-        $('#tempC, #tempF').html('Currently Unavailable');
-      }
-      //Background color based on temp, per rangeOfColors above
-      $('body').css("background-color", rangeOfColors(temp));
-
-      //Icon
-      var icon = report.weather[0].icon;
-      var iconLink = "http://openweathermap.org/img/w/" + icon + ".png";
-      if (icon !== undefined) {
-        $('#icon').html("<img src=" + iconLink + ">");
-      } else {
-        $('#icon').html('');
-      }
-      //Description - Custom function capitalizes each word's first letter
-      function titleCase(str) {
-        str = str.toLowerCase().split(' ');
-        for (var i = 0; i < str.length; i++) {
-          str[i] = str[i].replace(str[i][0], str[i][0].toUpperCase());
+        //Country - only Korea will be spelled out
+        function spell(val) {
+          var a = "";
+          if (val === 'KR') {
+            a = 'South Korea';
+          } else {
+            a = val;
+          }
+          return a;
         }
-        str = str.join(' ');
-        return str;
-      }
 
-      var description = titleCase(report.weather[0].description);
-      if (description !== undefined) {
-        $('#description').html(description);
-      } else {
-        $('#description').html('');
-      }
-      //Wind
-      //Wind Speed
-      var windSpeed = report.wind.speed;
-      if (windSpeed !== undefined) {
-        $('#windSpeedM').html(windSpeed.toFixed(1) + " m/s, ");
-        $('#windSpeedI').html((windSpeed * 2.23694).toFixed(1) + " mph, ");
-      } else {
-        $('#windSpeedM, #windSpeedI').html('Currently Unavailable');
-      }
-      //Wind Direction - Custom function for the direction because the one from openweathermap.org is sometimes undefined
-      var windDeg = Math.round(report.wind.deg);
-
-      function compass(val) {
-        switch (true) {
-          case (((val > 348.75) && (val <= 360)) || ((val >= 0) && (val <= 11.25))):
-            answer = val + "&deg N";
-            break;
-          case ((val > 11.25) && (val <= 33.75)):
-            answer = val + "&deg NNE";
-            break;
-          case ((val > 33.75) && (val <= 56.25)):
-            answer = val + "&deg NE";
-            break;
-          case ((val > 56.25) && (val <= 78.75)):
-            answer = val + "&deg ENE";
-            break;
-          case ((val > 78.75) && (val <= 101.25)):
-            answer = val + "&deg E";
-            break;
-          case ((val > 101.25) && (val <= 123.75)):
-            answer = val + "&deg ESE";
-            break;
-          case ((val > 123.75) && (val <= 146.25)):
-            answer = val + "&deg SE";
-            break;
-          case ((val > 146.25) && (val <= 168.75)):
-            answer = val + "&deg SSE";
-            break;
-          case ((val > 168.75) && (val <= 191.25)):
-            answer = val + "&deg S";
-            break;
-          case ((191.25 > val) && (val <= 213.75)):
-            answer = val + "&deg SSW";
-            break;
-          case ((213.75 > val) && (val <= 236.25)):
-            answer = val + "&deg SW";
-            break;
-          case ((val > 236.25) && (val <= 258.75)):
-            answer = val + "&deg WSW";
-            break;
-          case ((val > 258.75) && (val <= 281.25)):
-            answer = val + "&deg W";
-            break;
-          case ((val > 281.25) && (val <= 303.75)):
-            answer = val + "&deg WNW";
-            break;
-          case ((val > 303.75) && (val <= 326.25)):
-            answer = val + "&deg NW";
-            break;
-          case ((val > 326.25) && (val <= 348.75)):
-            answer = val + "&deg NNW";
-            break;
-          default:
-            answer = "Not Calculated";
-            break;
+        var country = spell(report.sys.country);
+        if (country !== undefined) {
+          $('#country').html(country);
+        } else {
+          $('#country').html('Location Not Detected');
         }
-        return answer;
-      }
-      $('#windDeg').html(compass(windDeg));
+        //Temperature
+        var temp = Math.round(report.main.temp - 273.15);
 
-      //Pressure
-      var pressure = report.main.pressure;
-      if (pressure !== undefined) {
-        $('#pressureM').html(pressure.toFixed(1) + " mb");
-        $('#pressureI').html((pressure * 0.03).toFixed(1) + " in");
-      } else {
-        $('#pressureM, #pressureI').html('Currently Unavailable');
-      }
-      //Humidity
-      var humidity = report.main.humidity;
-      if (humidity !== undefined) {
-        $('#humidity').html(humidity + '%');
-      } else {
-        $('#humidity').html('Currently Unavailable');
-      }
-      //Visibility
-      var visibility = report.visibility;
-      if (visibility !== undefined) {
-        $('#visibilityM').html(visibility + ' m');
-        $('#visibilityI').html(Math.round((visibility * 3.281)) + ' ft');
-      } else {
-        $('#visibilityM, #visibilityI').html('Currently Unavailable');
-      }
-      //Clouds
-      var clouds = report.clouds.all;
-      if (clouds !== undefined) {
-        $('#clouds').html(clouds + "%");
-      } else {
-        $('#clouds').html('Currently Unavailable');
-      }
+        if (temp !== undefined) {
+
+          $('#tempC').html(temp + "&deg");
+          $('#tempF').html(Math.round(temp * 1.8 + 32) + "&deg");
+          $('#buttonM').html("C");
+          $('#buttonI').html("F");
+        } else {
+          $('#tempC, #tempF').html('Currently Unavailable');
+        }
+        //Background color based on temp, per rangeOfColors above
+        $('body').css("background-color", rangeOfColors(temp));
+
+        //Icon
+        var icon = report.weather[0].icon;
+        var iconLink = "http://openweathermap.org/img/w/" + icon + ".png";
+        if (icon !== undefined) {
+          $('#icon').html("<img src=" + iconLink + ">");
+        } else {
+          $('#icon').html('');
+        }
+        //Description - Custom function capitalizes each word's first letter
+        function titleCase(str) {
+          str = str.toLowerCase().split(' ');
+          for (var i = 0; i < str.length; i++) {
+            str[i] = str[i].replace(str[i][0], str[i][0].toUpperCase());
+          }
+          str = str.join(' ');
+          return str;
+        }
+
+        var description = titleCase(report.weather[0].description);
+        if (description !== undefined) {
+          $('#description').html(description);
+        } else {
+          $('#description').html('');
+        }
+        //Wind
+        //Wind Speed
+        var windSpeed = report.wind.speed;
+        if (windSpeed !== undefined) {
+          $('#windSpeedM').html(windSpeed.toFixed(1) + " m/s, ");
+          $('#windSpeedI').html((windSpeed * 2.23694).toFixed(1) + " mph, ");
+        } else {
+          $('#windSpeedM, #windSpeedI').html('Currently Unavailable');
+        }
+        //Wind Direction - Custom function for the direction because the one from openweathermap.org is sometimes undefined
+        var windDeg = Math.round(report.wind.deg);
+
+        function compass(val) {
+          switch (true) {
+            case (((val > 348.75) && (val <= 360)) || ((val >= 0) && (val <= 11.25))):
+              answer = val + "&deg N";
+              break;
+            case ((val > 11.25) && (val <= 33.75)):
+              answer = val + "&deg NNE";
+              break;
+            case ((val > 33.75) && (val <= 56.25)):
+              answer = val + "&deg NE";
+              break;
+            case ((val > 56.25) && (val <= 78.75)):
+              answer = val + "&deg ENE";
+              break;
+            case ((val > 78.75) && (val <= 101.25)):
+              answer = val + "&deg E";
+              break;
+            case ((val > 101.25) && (val <= 123.75)):
+              answer = val + "&deg ESE";
+              break;
+            case ((val > 123.75) && (val <= 146.25)):
+              answer = val + "&deg SE";
+              break;
+            case ((val > 146.25) && (val <= 168.75)):
+              answer = val + "&deg SSE";
+              break;
+            case ((val > 168.75) && (val <= 191.25)):
+              answer = val + "&deg S";
+              break;
+            case ((191.25 > val) && (val <= 213.75)):
+              answer = val + "&deg SSW";
+              break;
+            case ((213.75 > val) && (val <= 236.25)):
+              answer = val + "&deg SW";
+              break;
+            case ((val > 236.25) && (val <= 258.75)):
+              answer = val + "&deg WSW";
+              break;
+            case ((val > 258.75) && (val <= 281.25)):
+              answer = val + "&deg W";
+              break;
+            case ((val > 281.25) && (val <= 303.75)):
+              answer = val + "&deg WNW";
+              break;
+            case ((val > 303.75) && (val <= 326.25)):
+              answer = val + "&deg NW";
+              break;
+            case ((val > 326.25) && (val <= 348.75)):
+              answer = val + "&deg NNW";
+              break;
+            default:
+              answer = "Not Calculated";
+              break;
+          }
+          return answer;
+        }
+        $('#windDeg').html(compass(windDeg));
+
+        //Pressure
+        var pressure = report.main.pressure;
+        if (pressure !== undefined) {
+          $('#pressureM').html(pressure.toFixed(1) + " mb");
+          $('#pressureI').html((pressure * 0.03).toFixed(1) + " in");
+        } else {
+          $('#pressureM, #pressureI').html('Currently Unavailable');
+        }
+        //Humidity
+        var humidity = report.main.humidity;
+        if (humidity !== undefined) {
+          $('#humidity').html(humidity + '%');
+        } else {
+          $('#humidity').html('Currently Unavailable');
+        }
+        //Visibility
+        var visibility = report.visibility;
+        if (visibility !== undefined) {
+          $('#visibilityM').html(visibility + ' m');
+          $('#visibilityI').html(Math.round((visibility * 3.281)) + ' ft');
+        } else {
+          $('#visibilityM, #visibilityI').html('Currently Unavailable');
+        }
+        //Clouds
+        var clouds = report.clouds.all;
+        if (clouds !== undefined) {
+          $('#clouds').html(clouds + "%");
+        } else {
+          $('#clouds').html('Currently Unavailable');
+        }
+      });
     });
+  }
 
-    //Metric/Imperial button toggle
-    $('#buttonM, #buttonI').click(function() {
-      $('#tempC, #tempF').toggleClass('goAway');
-      $('#windSpeedM, #windSpeedI').toggleClass('goAway');
-      $('#visibilityM, #visibilityI').toggleClass('goAway');
-      $('#pressureM, #pressureI').toggleClass('goAway');
-      $('#buttonM, #buttonI').toggleClass('goAway');
-    });
-
+  //Metric/Imperial button toggle
+  $('#buttonM, #buttonI').click(function() {
+    $('#tempC, #tempF').toggleClass('goAway');
+    $('#windSpeedM, #windSpeedI').toggleClass('goAway');
+    $('#visibilityM, #visibilityI').toggleClass('goAway');
+    $('#pressureM, #pressureI').toggleClass('goAway');
+    $('#buttonM, #buttonI').toggleClass('goAway');
   });
+
 });
