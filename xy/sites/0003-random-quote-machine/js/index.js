9abf0815adc4ffc57918def4d46f9bef7458b8d5
CharmedSatyr <z@charmed.tech>
2016-12-24 09:13:20 +0900

    Replaced 2 quotes with shorter ones on site 0003

--- xy/sites/0003-random-quote-machine/js/index.js
+++ xy/sites/0003-random-quote-machine/js/index.js
@@ -257,12 +257,12 @@ var list = {
     author: "Author Unknown"
   },
   63: {
-    quotation: "What feeling feels like over time. An attempt to screw up what feeling feels like over time. Heartbreak and a high C.... The often welcome melodic lie.... The soul's undersong. The orchestration of randomness, a flirtation with the boundaries of silence and space.... a reminder that the self wants to disappear, be taken away from itself and returned.",
-    author: "Stephen Dunn"
+    quotation: "Be nice to nerds. You may end up working for them. We all could.",
+    author: "Charles J. Sykes"
   },
   64: {
-    quotation: "Musical compositions, it should be remembered, do not inhabit certain countries, certain museums, like paintings and statues. The Mozart Quintet is not shut up in Salzburg: I have it in my pocket.",
-    author: "Henri Rabaud"
+    quotation: "I think, that if the world were a bit more like ComicCon, it would be a better place.",
+    author: "Matt Smith"
   },
   65: {
     quotation: "Music is the poetry of the air.",
@@ -317,7 +317,7 @@ var list = {
     author: "Neil deGrasse Tyson"
   },
   78: {
-    quotation: "Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do.",
+    quotation: "Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently.",
     author: "Rob Siltanen"
   },
   79: {
