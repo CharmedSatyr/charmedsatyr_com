9567a9da0141b39cc0ce9f72930c9db2e669a940
CharmedSatyr <z@charmed.tech>
2017-02-20 14:05:13 +0900

    Fuller original PHP

--- contact.php
+++ contact.php
@@ -1,13 +1,27 @@
 <?php
 /* Set e-mail recipient */
-$myemailEntry  = "email@charmedsatyr.com";
+$myemail  = "email@charmedsatyr.com";
 
 /* Check all form inputs using check_input function */
-$nameEntry = check_input($_POST['nameEntry'], 'Enter your name');
-$subjectEntry  = check_input($_POST['subjectEntry'], 'Write a subjectEntry');
+$nameEntry = check_input($_POST['nameEntry'], "Enter your name");
+$subjectEntry  = check_input($_POST['subjectEntry'], "Write a subject");
 $emailEntry    = check_input($_POST['emailEntry']);
 $websiteEntry  = check_input($_POST['websiteEntry']);
-$messageEntry = check_input($_POST['messageEntry'], 'Write your message');
+$likeit   = check_input($_POST['likeit']);
+$how_find = check_input($_POST['how']);
+$messageEntry = check_input($_POST['messageEntry'], "Write your message");
+
+/* If e-mail is not valid show error message */
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $emailEntry))
+{
+    show_error("E-mail address not valid");
+}
+
+/* If URL is not valid set $websiteEntry to empty */
+if (!preg_match("/^(https?:\/\/+[\w\-]+\.[\w\-]+)/i", $websiteEntry))
+{
+    $websiteEntry = '';
+}
 
 /* Let's prepare the message for the e-mail */
 $message = "Hello!
@@ -18,14 +32,17 @@ Name: $nameEntry
 E-mail: $emailEntry
 URL: $websiteEntry
 
-Message:
+Like the website? $likeit
+How did he/she find it? $how_find
+
+message:
 $messageEntry
 
 End of message
 ";
 
 /* Send the message using mail() function */
-mail($emailEntry, $subjectEntry, $messageEntry);
+mail($myemail, $subjectEntry, $message);
 
 /* Redirect visitor to the thank you page */
 header('Location: thanks.html');
@@ -46,6 +63,16 @@ function check_input($data, $problem='')
 
 function show_error($myError)
 {
+?>
+    <html>
+    <body>
+
+    <b>Please correct the following error:</b><br />
+    <?php echo $myError; ?>
+
+    </body>
+    </html>
+<?php
 exit();
 }
 ?>
