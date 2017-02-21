5b677db49348ab583308afe23ea43a354e4d54fc
CharmedSatyr <z@charmed.tech>
2017-02-21 10:07:46 +0900

    Changed colors to site variables

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 //Initiates and controls queryloader2.min.js
 window.addEventListener('DOMContentLoaded', function() {QueryLoader2(document.querySelector("body"), {
-  barColor: "#efefef",
-  backgroundColor: "#111",
+  barColor: "#ffffff",
+  backgroundColor: "#202020",
   percentage: true,
   barHeight: 1,
-  minimumTime: 200,
+  minimumTime: 0,
   fadeOutTime: 1000
 });
 });
