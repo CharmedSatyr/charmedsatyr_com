88e14c0afc6f77c8ea7d2ac25f056221de807af1
CharmedSatyr <z@charmed.tech>
2017-10-08 13:29:24 +0900

    Pug properly renders templated views using gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,51 @@ gulp.task('index', function buildHTML() {
     .pipe(concat('index.html'))
     .pipe(gulp.dest('./'))
 })
+
+gulp.task('thanks', function buildHTML() {
+  return gulp
+    .src('thanksAndError.pug')
+    .pipe(
+      pug({
+        data: {
+          thanksTop: 'Thanks For Your Message!',
+          thanksSub: "I'll be in touch."
+        }
+      })
+    )
+    .pipe(concat('thanks.html'))
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('missing', function buildHTML() {
+  return gulp
+    .src('thanksAndError.pug')
+    .pipe(
+      pug({
+        data: {
+          missingTop: 'Page Not Found',
+          missingSub: 'Please check the address.'
+        }
+      })
+    )
+    .pipe(concat('error404.html'))
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('fiveOhOne', function buildHTML() {
+  return gulp
+    .src('thanksAndError.pug')
+    .pipe(
+      pug({
+        data: {
+          fiveOhOneTop: 'There Seems To Be A Problem',
+          fiveOhOneSub: 'Please check the address.'
+        }
+      })
+    )
+    .pipe(concat('error501.html'))
+    .pipe(gulp.dest('./'))
+})
+
+//All tasks
+gulp.task('default', ['index', 'thanks', 'missing', 'fiveOhOne'])
